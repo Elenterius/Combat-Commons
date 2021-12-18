e23dcf3f5f5a2ffb1fee09707f24b755ceb6625b
Elenterius <elenterius@users.noreply.github.com>
2021-12-18 02:52:35 +0100

    added nonnull annotations

---
+++ src/main/java/com/github/elenterius/combat_commons/compat/pehkui/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.github.elenterius.combat_commons.compat.pehkui;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
